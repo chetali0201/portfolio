7a7f3d6ef43945aa5082752d919cef21868f0f03
Chetali Kumbhare <chetalikumbhare0212@gmail.com>
2026-06-21 00:31:45 +0530

    github pages deploy fix

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -3,12 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+   base: "/portfolio/",
   build: {
-    outDir: 'dist',
-    sourcemap: false,
-    minify: 'esbuild',
-  },
-  server: {
-    port: 5173,
+    outDir: "dist",
   },
 })
